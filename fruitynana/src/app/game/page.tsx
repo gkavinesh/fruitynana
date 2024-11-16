019335295f2693cd69ed1536d4038ceff9e83c0d
kavinesh <gkavinesh186@gmail.com>
2024-11-17 00:31:19 +0530

    created an interface for the game itself

---
+++ fruitynana/src/app/game/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import Game from '../../components/Game/game';
+import Preloader from '../../components/preloader/preloader'; // Import the Preloader component
+
+const SignUpPage = () => {
+  const [loading, setLoading] = useState(true);
+
+  // Simulate a loading effect (you can adjust the duration or use a real loading trigger)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // 3 seconds
+
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="w-full max-w-md">
+        <Game />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
